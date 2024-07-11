d17d3b2951f94effc05636a24614c580e7e78bf6
Egal97 <egallagher97@gmail.com>
2024-07-11 14:25:40 +0100

    Front End Code for Displaying all Delivery Employees

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 import session from "express-session";
 
 import { getAllDatabases } from "./controllers/TestController";
+import { getAllDeliveryEmployees } from "./controllers/DeliveryEmployeeController";
 
 const app = express();
 
@@ -29,4 +30,4 @@ app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
 
-app.get('/', getAllDatabases);
+app.get('/deliveryEmployees', getAllDeliveryEmployees);
